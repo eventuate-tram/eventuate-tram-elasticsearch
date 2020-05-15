ab0ee7bf977f38436ad853396f29fcea11ae2750
Didier Alberto Tabares Higuita <dtabaresh@psl.com.co>
2020-05-15 15:29:22 -0500

    Configures kafka consumer factory

--- eventuate-tram-spring-jdbc-kafka-elasticsearch/src/main/java/io/eventuate/tram/spring/jdbckafka/elasticsearch/TramJdbcKafkaElasticsearchConfiguration.java
+++ eventuate-tram-spring-jdbc-kafka-elasticsearch/src/main/java/io/eventuate/tram/spring/jdbckafka/elasticsearch/TramJdbcKafkaElasticsearchConfiguration.java
@@ -2,7 +2,7 @@ package io.eventuate.tram.spring.jdbckafka.elasticsearch;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import io.eventuate.tram.consumer.kafka.elasticsearch.ElasticsearchKafkaConsumerConfigurerConfiguration;
+import io.eventuate.tram.consumer.kafka.elasticsearch.ElasticsearchKafkaConsumerFactorySpringConfiguration;
 import io.eventuate.tram.consumer.kafka.elasticsearch.EventuateKafkaConsumerElasticsearchSpringConfigurationPropertiesConfiguration;
 import io.eventuate.tram.spring.consumer.elasticsearch.ElasticsearchConsumerSpringConfigurationPropertiesConfiguration;
 import io.eventuate.tram.spring.consumer.elasticsearch.TramConsumerElasticsearchAutoConfiguration;
@@ -11,12 +11,12 @@ import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcC
 
 @Configuration
 @Import({
-         TramConsumerElasticsearchAutoConfiguration.class,
-         TramMessageProducerJdbcConfiguration.class,
-         EventuateTramKafkaMessageConsumerConfiguration.class,
-         ElasticsearchKafkaConsumerConfigurerConfiguration.class,
-         ElasticsearchConsumerSpringConfigurationPropertiesConfiguration.class,
-         EventuateKafkaConsumerElasticsearchSpringConfigurationPropertiesConfiguration.class,
+    TramConsumerElasticsearchAutoConfiguration.class,
+    TramMessageProducerJdbcConfiguration.class,
+    EventuateTramKafkaMessageConsumerConfiguration.class,
+    ElasticsearchKafkaConsumerFactorySpringConfiguration.class,
+    ElasticsearchConsumerSpringConfigurationPropertiesConfiguration.class,
+    EventuateKafkaConsumerElasticsearchSpringConfigurationPropertiesConfiguration.class,
 })
 public class TramJdbcKafkaElasticsearchConfiguration {
 }
